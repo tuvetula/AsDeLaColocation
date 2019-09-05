137aa7530d1c3acb0c00cf12fa4bd8de9f9c8954
tuvetula <romain.marcant@gmail.com>
2019-09-05 15:57:13 +0200

    rajout fichier pour puppeteer

--- view/backEnd/displayHomeUser.php
+++ view/backEnd/displayHomeUser.php
@@ -2,10 +2,16 @@
 $title = "Bienvenue";
 ob_start();
 ?>
+<div class="container">
+    <div id="myadvertisements" class="jumbotron">
+
+    </div>
+
+</div>
 
 
 
-
+<script src="public/js/displayUserAdvertisement.js"></script>
 <?php
 $content = ob_get_clean();
 require('view/includes/template.php');
--- view/includes/template.php
+++ view/includes/template.php
@@ -14,9 +14,6 @@
 <body>
     <?php include('view/includes/navbar.php');?>
     <?=$content?>
-    <script src="public/js/energy.js"></script>
-    <script src="public/js/uploadFile.js"></script>
-    
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
