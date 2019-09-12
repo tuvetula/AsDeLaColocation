9723f7f040693adb12cdea6eac5899af3f7eb025
tuvetula <romain.marcant@gmail.com>
2019-09-12 12:57:24 +0200

    navbar Ok

--- view/includes/navbar.php
+++ view/includes/navbar.php
@@ -9,63 +9,41 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="index.php">Accueil</a>
                 </li>
-
                 <?php
-      if (isset($_SESSION['mail']) && isset($_SESSION['isAdmin'])) {
-          // <!--Menu Utilisateur connecté et non-admin-->
-          if (!$_SESSION['isAdmin']) {?>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php?page=homeUser">Accueil</a>
-                </li>
+                if (isset($_SESSION['mail']) && isset($_SESSION['isAdmin'])) {
+                    // <!--Menu Utilisateur connecté et non-admin-->?>
                 <li class="nav-item">
                     <a class="nav-link" href="index.php?page=myAdvertisements">Mes annonces</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="index.php?page=displayAddAnAdvertisement">Déposer une annonce</a>
                 </li>
+                <?php 
+                    if ($_SESSION['isAdmin']) {
+                        //Menu utilisateur connecté et admin
+                        ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Utilisateurs</a>
+                    </li>
+                    <?php
+                        } 
+                        ?>
                 <li class="nav-item">
                     <a class="nav-link" href="index.php?page=displayMyAccount">Mon compte</a>
                 </li>
+                <?php
+                }
+                ?>
             </ul>
-            <a href="index.php?disconnect=1" class="btn btn btn-danger pull-right" role="button" aria-pressed="true">Se
-                déconnecter</a>
-
             <?php
-        } else {
-            //Menu utilisateur connecté et admin
-          ?>
-            <li class="nav-item">
-                <a class="nav-link" href="index.php?page=homeUser">Accueil</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="index.php?page=myAdvertisements">Mes annonces</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="index.php?page=displayAddAnAdvertisement">Déposer une annonce</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="index.php?page=displayMyAccount">Mon compte</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Utilisateurs</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="index.php">Mon compte</a>
-            </li>
-
-            </ul>
+            if (isset($_SESSION['mail']) && isset($_SESSION['isAdmin'])) {
+            ?>
             <a href="index.php?disconnect=1" class="btn btn btn-danger pull-right" role="button" aria-pressed="true">Se
                 déconnecter</a>
             <?php
-        }
-      } else {
-          //Menu utilisateur non-connecté
-        ?>
-            </ul>
-            <?php
-      }
+            }
             ?>
         </div>
     </nav>
