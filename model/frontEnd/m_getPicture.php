fc43de3d50de418c7c96980540e1544f5155accf
tuvetula <romain.marcant@gmail.com>
2019-09-15 23:37:04 +0200

    recup picture-order pour assignation ajout photos

--- model/frontEnd/m_getPicture.php
+++ model/frontEnd/m_getPicture.php
@@ -1,8 +1,8 @@
 <?php
-
+//Récupère la photo d'une annonce qui a le picture_order 1
 function getAdvertisementPictureOrder1($advertisementId){
     $db = connectBdd();
-    $request = $db->query('SELECT picture_fileName FROM pictures WHERE advertisement_id="'.$advertisementId.'" AND picture_order="1"');
+    $request = $db->query('SELECT picture_fileName FROM pictures WHERE advertisement_id='.$advertisementId.' AND picture_order=1');
     $pictureFilename = $request->fetch(PDO::FETCH_ASSOC);
     $request->closeCursor();
     return $pictureFilename['picture_fileName'];
@@ -12,8 +12,18 @@ function getAdvertisementPictureOrder1($advertisementId){
 function getAdvertisementPictures($advertisementId){
     $advertisementId = htmlspecialchars(strip_tags($advertisementId));
     $db = connectBdd();
-    $request = $db->query('SELECT picture_id,picture_fileName,picture_order FROM pictures WHERE advertisement_id="'.$advertisementId.'" ORDER BY picture_order');
+    $request = $db->query('SELECT picture_id,picture_fileName,picture_order FROM pictures WHERE advertisement_id='.$advertisementId.' ORDER BY picture_order');
     $picturesRequest = $request->fetchAll(PDO::FETCH_ASSOC);
     $request->closeCursor();
     return $picturesRequest;
+}
+
+//Récupère le dernier picture_order des photos d'une annonce
+function getLastPictureOrder($advertisementId){
+    $advertisementId = htmlspecialchars(strip_tags($advertisementId));
+    $db = connectBdd();
+    $request = $db->query('SELECT picture_order FROM pictures WHERE advertisement_id='.$advertisementId.' ORDER BY picture_order DESC');
+    $picturesRequest = $request->fetch(PDO::FETCH_ASSOC);
+    $request->closeCursor();
+    return $picturesRequest['picture_order'];
 }
