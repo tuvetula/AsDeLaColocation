17bc1c7b329cf5c24f5a574386650ca880c14710
tuvetula <romain.marcant@gmail.com>
2019-09-23 15:47:22 +0200

    robots

---
+++ model/ajax/m_modifyIsActive.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+require_once('../bdd/config.php');
+if (isset($_SESSION['mail'])){
+
+    $advertisementId = htmlspecialchars(strip_tags($_POST['advertisementId']));
+    
+    //Connexion Base de donnée
+    try {
+        $bdd = new PDO($acces.$dbName,$login,$password,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    } catch (Exception $e) {
+        die('Erreur : '.$e->getMessage());
+    }
+    
+    //Requete recup etat actuel de advertisement_isActive
+    $request=$bdd->query('SELECT advertisement_isActive FROM advertisements 
+                        WHERE advertisement_id="'.$advertisementId.'"');
+    $advertisementIsActiveState = $request->fetchAll(PDO::FETCH_ASSOC);
+    $request->closeCursor();
+    
+    //préparation nouvel état de isActive à insérer en bdd
+    if($advertisementIsActiveState[0]['advertisement_isActive']){
+        $advertisementIsActiveNewState = 0;
+    }else{
+        $advertisementIsActiveNewState = 1;
+    }
+    
+    //Requete changement valeur de isActive
+    $insert=$bdd->prepare('UPDATE advertisements SET advertisement_isActive="'.$advertisementIsActiveNewState.'" WHERE advertisement_id="'.$advertisementId.'"');
+    $insert->execute();
+}
