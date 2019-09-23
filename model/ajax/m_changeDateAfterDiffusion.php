0b0799dd54c2a9ffbc039b37f4056d17b01edd37
tuvetula <romain.marcant@gmail.com>
2019-09-23 15:47:22 +0200

    robots

---
+++ model/ajax/m_changeDateAfterDiffusion.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: application/javascript');
+require_once('../bdd/config.php');
+if (isset($_GET['id']) && $_GET['id'] == $idjsonReturn){
+    $columnBdd = htmlspecialchars(strip_tags($_GET['columnBdd']));
+    $advertisementId = htmlspecialchars(strip_tags($_GET['advertisementId']));
+    $dateValue = htmlspecialchars(strip_tags($_GET['dateValue']));
+    if($dateValue == "publication"){
+        $date = '"'.date("Y-m-d").'"';
+    }else{
+        $date = "NULL";
+    }
+    //Connexion Base de donnée
+    try {
+        $bdd = new PDO($acces.$dbName,$login,$password,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    } catch (Exception $e) {
+        die('Erreur : '.$e->getMessage());
+    }
+    //Requete changement valeur de dateOfLastDiffusion
+    $insert=$bdd->prepare('UPDATE advertisements SET '.$columnBdd.'='.$date.' WHERE advertisement_id="'.$advertisementId.'"');
+    $insert->execute();
+    echo true;
+}
