e1c5bc0f8d38169ce4d63930e65ff6ce13659cae
tuvetula <romain.marcant@gmail.com>
2019-10-15 21:02:18 +0200

    modif advertisment

--- model/frontEnd/m_deleteAdvertisement.php
+++ model/frontEnd/m_deleteAdvertisement.php
@@ -1,15 +1,15 @@
 <?php
 require_once('model/bdd/bddConfig.php');
-require_once('model/frontEnd/m_getPicture.php');
-require_once('model/frontEnd/m_getAdvertisement.php');
-require_once('model/frontEnd/m_getUser.php');
-
-
 //Supprime une annonce avec son id et les photos liées à l'annonce
 function deleteAdvertisementBdd($advertisementId){
-    $advertisementIdToDeleteBdd = htmlspecialchars(strip_tags($advertisementId));
     $db = connectBdd();
-    $delete = $db->prepare('DELETE FROM advertisements WHERE advertisement_id="'.$advertisementId.'"');
-    $delete->execute();
+    $delete = $db->prepare('UPDATE advertisements SET 
+    advertisement_isRegister=:isRegister,
+    advertisement_isActive=:isActive
+    WHERE advertisement_id="'.$advertisementId.'"');
+    $delete->execute(array(
+        ':isRegister'=> 0,
+        ':isActive'=> 0
+    ));
     return true;
 }
