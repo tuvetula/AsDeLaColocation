ec19aa06d9e6c13732823721c746b72d45ca8bd2
tuvetula <romain.marcant@gmail.com>
2019-10-21 22:07:17 +0200

    spinnerbutton

---
+++ public/js/spinnerSubmitButton.js
@@ -0,0 +1,9 @@
+function spinnerSubmitButton() {
+    let submitButton = document.getElementById('submitButton');
+    let spinner = document.createElement('span');
+    let classListSpinner = spinner.classList;
+    classListSpinner.add('spinner-border');
+    classListSpinner.add('spinner-border-sm');
+    submitButton.innerText = 'Chargement...';
+    submitButton.appendChild(spinner);
+}
