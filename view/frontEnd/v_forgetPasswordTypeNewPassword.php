b41988e5fed049329836d11ccb7c487df18a3c76
tuvetula <romain.marcant@gmail.com>
2019-11-20 13:06:00 +0100

    modif forgetPassword

--- view/frontEnd/v_forgetPasswordTypeNewPassword.php
+++ view/frontEnd/v_forgetPasswordTypeNewPassword.php
@@ -6,34 +6,41 @@ ob_start();
     <div class="jumbotron">
         <h1 class="pb-3 text-center">Réinitialisation mot de passe</h1>
         <div class="container col-md-9 col-lg-6">
-            <form method="post" action="index.php" class="text-center">
-            <input type="hidden" name="userMail" value="<?=$mail?>">
-            <?php if (isset($token)){?>
-            <input type="hidden" name="userToken" value="<?=$token?>">
-            <?php } ?>
-            <?php if (isset($_SESSION['mail'])){
-                ?>
+            <form method="post" action="index.php" class="text-center" onsubmit="spinnerSubmitButton()">
+                <input type="hidden" name="userMail" value="<?=$mail?>">
+                <?php if (isset($token)){?>
+                <input type="hidden" name="userToken" value="<?=$token?>">
+                <?php } ?>
+                <?php if (isset($_SESSION['mail'])){ ?>
                 <div class="form-group text-left">
                     <label class="font-weight-bold" for="oldPassword">Ancien mot de passe</label>
                     <input type="password" class="form-control form-control-lg" id="oldPassword" name="oldPassword"
                         placeholder="Saisir votre ancien mot de passe" maxlength="255" required>
+                <?php if (isset($fillingError['oldPassword'])){ ?>
+                        <p class="text-danger font-weight-bold mb-0 pb-1" type="error"><?=$fillingError['oldPassword']?></p>
+                        <?php } ?>
                 </div>
-
                 <?php
-            }
-            ?>
+                }
+                ?>
                 <div class="form-group text-left">
                     <label class="font-weight-bold" for="passwordReinitialization1">Nouveau mot de passe</label>
                     <input type="password" class="form-control form-control-lg" id="passwordReinitialization1" name="passwordReinitialization1"
                         placeholder="Saisir un nouveau mot de passe" maxlength="255" required>
+                    <?php if (isset($fillingError['passwordReinitialization1'])){ ?>
+                    <p class="text-danger font-weight-bold mb-0 pb-1" type="error"><?=$fillingError['passwordReinitialization1']?></p>
+                    <?php } ?>
                 </div>
                 <div class="form-group text-left">
                     <label class="font-weight-bold" for="passwordReinitialization2">Confirmation du nouveau mot de passe</label>
                     <input type="password" class="form-control form-control-lg" id="passwordReinitialization2" name="passwordReinitialization2"
                         placeholder="Confirmer le nouveau mot de passe" maxlength="255" required>
+                    <?php if (isset($fillingError['passwordReinitialization1'])){ ?>
+                    <p class="text-danger font-weight-bold mb-0 pb-1" type="error"></p>
+                    <?php } ?>
                 </div>
                 <div class="container">
-                    <button type="submit" class="btn btn-primary">Soumettre</button>
+                    <button id="submitButton" type="submit" class="btn btn-primary">Soumettre</button>
                 </div>
                 <?php
                     if (isset($error) && !empty($error)){
@@ -44,7 +51,8 @@ ob_start();
         </div>
     </div>
 </div>
-</div>
+<script src="public/js/spinnerSubmitButton.js"></script>
+<script src="public/js/redBorder.js"></script>
 <?php
 $content = ob_get_clean();
 require('view/includes/template.php');
