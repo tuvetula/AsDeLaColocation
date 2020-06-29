ae3c6b4f172f455e62eedec57163e25e8b26e707
tuvetula <romain.marcant@gmail.com>
2020-06-29 16:02:45 +0200

    modif gitignore

---
+++ model/bdd/config.php
@@ -0,0 +1,16 @@
+<?php
+//Informations connexion base de donnée
+$acces = 'mysql:host=localhost;';
+$dbName = 'dbname=asdelacolocation;charset=utf8';
+$login = 'root';
+$password = '';
+//Lien mail pour recup de mot de passe
+$mailLinkToSend = 'http://localhost/AsDeLaColoc/index.php?';
+//Adresse du site
+$addressSite = 'localhost/asdelacoloc.fr';
+//Mail pour recevoir mail "Nouvelle inscription'
+$ownMail = "romain.marcant@gmail.com";
+//Id get json ajax
+$idJson = '@s2LaColoc@tion_Json31';
+//Id return robots ajax
+$idjsonReturn = '31R0b0tsComeB@ck';
